d64fbf8495606d16e349a775f6f360cf83354b42
IsakovOleg <162922795+IsakovOleg@users.noreply.github.com>
2024-11-25 18:31:21 +0300

    Update task1.sql

--- task1.sql
+++ task1.sql
@@ -9,12 +9,17 @@ DECLARE
     max_salary_value NUMERIC;
     is_existing_job BOOLEAN;
 BEGIN
+    IF new_job_id IS NULL OR new_job_title IS NULL OR new_min_salary IS NULL THEN
+        RAISE EXCEPTION 'cannot be NULL',
+            new_job_id, new_job_title, new_min_salary;
+    END IF;
+
     SELECT EXISTS (
         SELECT 1 FROM jobs WHERE job_id = new_job_id
     ) INTO is_existing_job;
 
     IF is_existing_job THEN
-        RAISE NOTICE 'already exists', new_job_id;
+        RAISE NOTICE 'already exist', new_job_id;
 
         UPDATE jobs
         SET job_title = new_job_title,
@@ -28,7 +33,13 @@ BEGIN
         INSERT INTO jobs (job_id, job_title, min_salary, max_salary)
         VALUES (new_job_id, new_job_title, new_min_salary, max_salary_value);
 
-        RAISE NOTICE 'successfully added', new_job_id;
+        RAISE NOTICE 'added', new_job_id;
     END IF;
 END;
 $$;
+
+
+CALL NEW_JOB('DEV_ENG', 'Development Engineer', 7000);
+CALL NEW_JOB('SY_ANAL', 'Updated Analyst', 8000);
+CALL NEW_JOB(NULL, 'Invalid Job', 5000);
+SELECT * FROM jobs WHERE job_id IN ('DEV_ENG', 'SY_ANAL');
