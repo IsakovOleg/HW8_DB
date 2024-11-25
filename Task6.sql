c7b80a0d8d3fd720c0bb0c0a79791dc8d612d30b
IsakovOleg <162922795+IsakovOleg@users.noreply.github.com>
2024-11-25 18:51:11 +0300

    Update Task6.sql

--- Task6.sql
+++ Task6.sql
@@ -11,7 +11,7 @@ BEGIN
     LIMIT 1;
 
     IF FOUND THEN
-        RAISE EXCEPTION 'exception',
+        RAISE EXCEPTION 'outside the range',
             NEW.job_id, invalid_employee.employee_id, invalid_employee.salary, NEW.min_salary, NEW.max_salary;
     END IF;
 
@@ -19,8 +19,25 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
 CREATE TRIGGER CHECK_SAL_RANGE
     BEFORE UPDATE OF min_salary, max_salary
     ON jobs
     FOR EACH ROW
 EXECUTE FUNCTION CHECK_SALARY_RANGE();
+
+UPDATE jobs
+SET min_salary = 6000, max_salary = 12000
+WHERE job_id = 'SY_ANAL';
+
+UPDATE jobs
+SET min_salary = 9000
+WHERE job_id = 'SY_ANAL';
+
+SELECT employee_id, salary, job_id
+FROM employees
+WHERE job_id = 'SY_ANAL';
+
+SELECT job_id, min_salary, max_salary
+FROM jobs
+WHERE job_id = 'SY_ANAL';
